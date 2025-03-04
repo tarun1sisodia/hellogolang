b4b2376e805d3195a9ac5d88270338bce09d7b7b
Tarun Sisodia <coccoder999@gmail.com>
2025-03-04 23:22:58 +0530

    some issue

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/tarun1sisodia/hellobygola/hellogolang
 
 go 1.24.0
+
+require github.com/tarun1sisodia/Golang v0.0.0-20250304172912-914ba29a1448
--- main.go
+++ main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	"fmt"
-	"log"
+    "fmt"
+    "log"
 
-	"github.com/tarun1siodia/Golang"
+    "github.com/tarun1sisodia/Golang"
 )
 
 func main() {
-	log.SetPrefix("greetings: ")
-	log.SetFlags(0)
-	message, err := greetings.Hello("Tarun")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(message)
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    // Request a greeting message.
+    message, err := greetings.Hello("Gladys")
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.
+    fmt.Println(message)
 }
