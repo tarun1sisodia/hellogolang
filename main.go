58bb8a14b1fec9df18163f5a4726248fc782077a
Tarun Sisodia <coccoder999@gmail.com>
2025-03-04 23:20:06 +0530

    some issue

--- main.go
+++ main.go
@@ -1,9 +1,18 @@
 package main
 
-import 
-(
+import (
 	"fmt"
+	"log"
 
-	"github.com/tarun1sisodia/Golang"
-	
+	"github.com/tarun1siodia/Golang"
 )
+
+func main() {
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+	message, err := greetings.Hello("Tarun")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
+}
